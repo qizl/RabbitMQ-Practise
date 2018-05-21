82c6e815a86ca7bde5b87b32ca2177c200ace645
qizl <q@qizl.cn>
2018-05-21 18:12:14 +0800

    -

---
+++ RabbitMQ_Pratise/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RabbitMQ_Pratise")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RabbitMQ_Pratise")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b822acd2-ae6e-434d-a30b-20ff5401a749")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ RabbitMQ_Pratise/Test.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RabbitMQ_Pratise
+{
+    [TestClass]
+    public class Test
+    {
+        private ConnectionFactory _factory = new ConnectionFactory() { HostName = "localhost", UserName = "test", Password = "123123" };
+
+        [TestMethod]
+        public void BasicPublish()
+        {
+            using (var connection = this._factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("hello", false, false, false, null);
+                    var body = Encoding.UTF8.GetBytes("Hello World");
+                    channel.BasicPublish("", "hello", null, body);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void Receive()
+        {
+            using (var connection = this._factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("hello", false, false, false, null);
+
+                    var consumer = new QueueingBasicConsumer(channel);
+                    channel.BasicConsume("hello", true, consumer);
+
+                    while (true)
+                    {
+                        var ea = (BasicDeliverEventArgs)consumer.Queue.Dequeue();
+                        var message = Encoding.UTF8.GetString(ea.Body);
+                    }
+                }
+            }
+        }
+    }
+}
