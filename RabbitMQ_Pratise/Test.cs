0bf298da8028c4f63dabd9207cad5a1dfb2e3ae7
qizl <q@qizl.cn>
2018-05-21 18:40:21 +0800

    持久化

--- RabbitMQ_Pratise/Test.cs
+++ RabbitMQ_Pratise/Test.cs
@@ -17,7 +17,11 @@ namespace RabbitMQ_Pratise
             {
                 using (var channel = connection.CreateModel())
                 {
-                    channel.QueueDeclare("hello", false, false, false, null);
+                    channel.QueueDeclare(queue: "hello", durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+                    var properties = channel.CreateBasicProperties();
+                    properties.Persistent = true;
+
                     var body = Encoding.UTF8.GetBytes("Hello World");
                     channel.BasicPublish("", "hello", null, body);
                 }
@@ -41,13 +45,16 @@ namespace RabbitMQ_Pratise
                     //    var ea = (BasicDeliverEventArgs)consumer.Queue.Dequeue();
                     //    var message = Encoding.UTF8.GetString(ea.Body);
                     //}
-                    channel.QueueDeclare("hello", false, false, false, null);
+                    channel.QueueDeclare(queue: "hello", durable: true, exclusive: false, autoDelete: false, arguments: null);
+                    channel.BasicQos(0, 1, false);
 
                     var consumer = new EventingBasicConsumer(channel);
                     consumer.Received += (model, ea) =>
                     {
                         var body = ea.Body;
                         var message = Encoding.UTF8.GetString(body);
+
+                        channel.BasicAck(ea.DeliveryTag, false);
                     };
                     channel.BasicConsume("hello", true, consumer);
                 }
